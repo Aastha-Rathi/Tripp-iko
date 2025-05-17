effe575770171ee0fb8c4ad1569cbacf044e38d7
Aastha-Rathi <aastharathi0404gmail.com>
2025-05-17 23:25:28 +0530

    updated serer.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const path = require('path');
+const { multerErrorHandler } = require("./utils/multer");
 const cors = require('cors');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json()); 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URL)
@@ -17,10 +19,14 @@ mongoose.connect(process.env.MONGO_URL)
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const travelStoryRoutes = require('./routes/travelStoryRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 // endpoints
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/travel-stories', travelStoryRoutes);
+app.use('/api/images', imageRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
